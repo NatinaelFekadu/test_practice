5cc0ec5d8309bf54b771acfea0a6bc9c5d31ac44
unknown <natnaelfekadu801@gmail.com>
2024-03-19 01:18:05 -0700

    docstring added

--- product.py
+++ product.py
@@ -1,5 +1,16 @@
 class Product:
     def __init__(self, name, price, quantity):
+        """
+        Initialize the object with the provided name, price, and quantity.
+
+        Parameters:
+            name (str): The name of the item.
+            price (float): The price of the item.
+            quantity (int): The quantity of the item.
+
+        Returns:
+            None
+        """
         self.name = name
         self.price = price
         self.quantity = quantity
